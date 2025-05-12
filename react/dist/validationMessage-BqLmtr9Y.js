fb4c167ecfd99d9c7cfac4f3fe2c9da651759d40
Justine <54615524+justinscoderepo@users.noreply.github.com>
2025-05-12 15:17:29 +0200

    Age B0oOS4L9

---
+++ react/dist/validationMessage-BqLmtr9Y.js
@@ -0,0 +1,1 @@
+import{j as e}from"./mainweb-h4V9e9SQ.js";function n({message:t,type:r}){return e.jsx("p",{className:"text-danger font-medium text-right text-sm mt-0",children:t&&t})}export{n as V};
