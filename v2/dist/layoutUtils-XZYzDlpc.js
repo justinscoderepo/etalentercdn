b344036b3908856d3433a23d855c78287b7f1e23
Azure Pipeline - CDN <pipeline@azure.com>
2026-01-30 18:23:07 +0000

    Deploy CDN assets from build 20260130.24 - 2026-01-30 18:23:07

---
+++ v2/dist/layoutUtils-XZYzDlpc.js
@@ -0,0 +1,1 @@
+import{j as r,r as a,_ as e}from"./main-DMKfhHuC.js";const i=a.lazy(()=>e(()=>import("./IframeRender-uHqm-o-a.js"),[])),n=a.lazy(()=>e(()=>import("./ConditionalLayoutWrapper-ChsgsjEN.js"),[])),s=(t,o={})=>r.jsx(n,{children:r.jsx(t,{...o})}),d=(t="Loading...",o={})=>s(i,{title:t,...o});export{s as a,d as w};
