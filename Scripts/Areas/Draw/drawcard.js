4f594a9c1027396ea23cafa3a0095664c2423384
Azure Pipeline - etalenterWeb CDN <pipeline@azure.com>
2026-07-29 11:46:49 +0000

    Deploy wwwroot assets from etalenterWeb build 20260729.1 - 2026-07-29 11:46:49

--- Scripts/Areas/Draw/drawcard.js
+++ Scripts/Areas/Draw/drawcard.js
@@ -42,15 +42,10 @@
     let selectedCompetitionItem = $("#competitionoptdropdown").find(
       "option:selected"
     );
-    let json = {};
-    if (selectedCompetitionItem.attr("data-json")) {
-      json = JSON.parse(selectedCompetitionItem.attr("data-json"));
-    }
     let drawBatches = parseInt($(this).val());
-    json.DrawBatches = drawBatches;
     let postData = {};
     postData.CompetitionStructureId = selectedCompetitionItem.val();
-    postData.JsonSettings = JSON.stringify(json);
+    postData.DrawSettingsJson = JSON.stringify({ DrawBatches: drawBatches });
 
     $.post("/CompetitionStructureJson/Save", postData, function (data) {
       alert("Draw Batches Updated");
