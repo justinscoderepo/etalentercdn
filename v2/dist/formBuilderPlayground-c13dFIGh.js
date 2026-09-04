f17cda702cba58e0789c5ac8347ef532d4f7b3bd
Azure Pipeline - CDN <pipeline@azure.com>
2026-09-04 07:10:26 +0000

    Deploy CDN assets from build 20260904.3 - 2026-09-04 07:10:26

---
+++ v2/dist/formBuilderPlayground-c13dFIGh.js
@@ -0,0 +1,1 @@
+import{j as r,o,_ as a}from"./main-BkeHKdMu.js";import{D as s,H as t}from"./index-DrEq7uJ-.js";import"./index-DF5JTaca.js";const d=o.lazy(()=>a(()=>import("./formBuilder-DJVkWiSL.js"),[])),n=e=>r.jsx(s,{backend:t,children:r.jsx("div",{className:"bg-surface p-5 flex ",children:r.jsx(d,{...e})})});export{n as default};
