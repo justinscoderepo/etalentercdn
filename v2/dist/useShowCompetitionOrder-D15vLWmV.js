0f0602a2b0b99550438bd9e092333afb49fa9f45
Azure Pipeline - CDN <pipeline@azure.com>
2026-09-19 14:18:53 +0000

    Deploy CDN assets from build 20260919.6 - 2026-09-19 14:18:53

---
+++ v2/dist/useShowCompetitionOrder-D15vLWmV.js
@@ -0,0 +1,1 @@
+import{t as r}from"./main-CERUsC41.js";const s=o=>Number(o?.AllowDisplayCompetitionOrder)===1,i=()=>{const{eventDetails:o,user:t}=r(),e=o?.AllowDisplayCompetitionOrder!=null?o:t;return s(e)};export{s as c,i as u};
