cbd4ad1222d67f971f49bddefa785004dba39ed7
Azure Pipeline - CDN <pipeline@azure.com>
2026-09-20 19:40:13 +0000

    Deploy CDN assets from build 20260920.1 - 2026-09-20 19:40:13

---
+++ v2/dist/formBuilderPlayground-DUp-MSro.js
@@ -0,0 +1,1 @@
+import{j as r,p as a,_ as o}from"./main-B3B8vX5o.js";import{D as s,H as t}from"./index-lM3OzK9-.js";import"./index-B7q3tm_9.js";const d=a.lazy(()=>o(()=>import("./formBuilder-DjoZAm02.js"),[])),n=e=>r.jsx(s,{backend:t,children:r.jsx("div",{className:"bg-surface p-5 flex ",children:r.jsx(d,{...e})})});export{n as default};
---
+++ v2/dist/useShowCompetitionOrder-BnpiDANv.js
@@ -0,0 +1,1 @@
+import{t as r}from"./main-B3B8vX5o.js";const s=o=>Number(o?.AllowDisplayCompetitionOrder)===1,i=()=>{const{eventDetails:o,user:t}=r(),e=o?.AllowDisplayCompetitionOrder!=null?o:t;return s(e)};export{s as c,i as u};
